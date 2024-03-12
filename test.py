a9a3fab023ce17a1139d53f6aaa11c1ffe7c52da
Мой профиль <91414139+ilyasrv@users.noreply.github.com>
2024-03-12 21:47:24 +0300

    Update

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ TOKEN = '6574304289:AAHbp-YuG9efM7lV--d9nOFawwGERdRZhmg'
 # Создаем экземпляр бота
 bot = telebot.TeleBot(TOKEN)
 
+# Словарь для хранения состояний пользователей (стек диалогов)
+user_states = {}
+
 # Словарь для хранения id пользователей
 users = {}
 
@@ -21,12 +24,12 @@ def start(message):
 # Функция для отображения начального менюff
 def display_start_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buy_button = types.KeyboardButton("Купить")
+    buy_button = types.KeyboardButton("Купить VPN")
     profile_button = types.KeyboardButton("Профиль")
     free_trial_button = types.KeyboardButton("Бесплатный тест")
     markup.add(buy_button, profile_button)
     markup.add(free_trial_button)
-    bot.send_message(message.chat.id, "Привет! Я бот для продажи подписки на VPN.", reply_markup=markup)
+    bot.send_message(message.chat.id, "👾 Приветствую! Я Ваш личный бот и помощник MaskVPN!\n \nЯ помогаю c обходом блокировок и защитой вашей конфиденциальности.", reply_markup=markup)
 
 # Функция для отображения профиля
 def display_profile(message):
@@ -47,38 +50,69 @@ def display_profile(message):
 def profile(message):
     display_profile(message)
 
+
+
 # Обработчик команды "Купить"
-@bot.message_handler(func=lambda message: message.text == "Купить")
+@bot.message_handler(func=lambda message: message.text == "Купить VPN")
 def buy(message):
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    button1 = types.InlineKeyboardButton("1 месяц - 350p", callback_data='buy_1_month')
-    button3 = types.InlineKeyboardButton("3 месяца - 840p", callback_data='buy_3_months')
-    button6 = types.InlineKeyboardButton("6 месяцев - 1500p", callback_data='buy_6_months')
-    button12 = types.InlineKeyboardButton("12 месяцев - 2000p", callback_data='buy_12_months')
-    markup.add(button1, button3, button6, button12)
+    markup = types.InlineKeyboardMarkup()
 
-    bot.send_message(message.chat.id, "Выберите тариф:", reply_markup=markup)
+    tariffs = [
+        {"months": 1, "price": 250},
+        {"months": 3, "price": 600},
+        {"months": 6, "price": 1000},
+        {"months": 12, "price": 1800}
+    ]
 
-# Обработчик команды "Бесплатный тест"
-@bot.message_handler(func=lambda message: message.text == "Бесплатный тест")
-def free_trial(message):
-    # Здесь можно добавить логику для предоставления бесплатного теста
-    bot.reply_to(message, "Вы выбрали Бесплатный тест. Наслаждайтесь бесплатным тестом VPN!")
+    for tariff in tariffs:
+        button_text = f"{tariff['months']} месяц{'а' if tariff['months'] != 1 else ''}\n{tariff['price']} ₽"
+        button = types.InlineKeyboardButton(button_text, callback_data=f"buy_{tariff['months']}_{tariff['price']}")
+        markup.add(button)
+    bot.send_message(message.chat.id, "Выберите удобный для вас тариф: \n\n\n\nК оплате принимаются карты РФ: Visa, MasterCard, МИР." , reply_markup=markup, parse_mode='HTML')
 
-# Обработчик нажатия на инлайн-кнопки
+# Обработчик нажатия на кнопки способов оплаты и тарифов
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     chat_id = call.message.chat.id
     user_id = call.from_user.id
 
+    # Обработка нажатия на кнопку выбора тарифа
     if call.data.startswith('buy_'):
-        months = int(call.data.split('_')[1])
-        # Здесь можно добавить логику для обработки покупки в зависимости от выбранного периода подписки
-        bot.send_message(chat_id, f"Вы выбрали тариф на {months} месяцев. Стоимость - {months * 10}$.")
+        data = call.data.split('_')
+        if len(data) == 3:
+            months = int(data[1])
+            price = int(data[2])
+       
+            # Создаем кнопки для способов оплаты
+            payment_markup = types.InlineKeyboardMarkup(row_width=4)
+            payment_markup.add(types.InlineKeyboardButton("Банковская карта", callback_data=f"payment_card_{months}_{price}"),
+                               types.InlineKeyboardButton("BTC", callback_data=f"payment_btc_{months}_{price}"),
+                               types.InlineKeyboardButton("Назад", callback_data=f"back"))
+            bot.send_message(chat_id, f"Вы выбрали тариф на <b>{months}</b> месяц{'а' if months == 1 else 'ев'}.\nСтоимость - <b>{price} рублей</b>.\n\nВыберите способ оплаты:", reply_markup=payment_markup, parse_mode='HTML')
 
-    elif call.data == "info":
-        # Здесь можно добавить логику для обработки нажатия на кнопку "Информация"
-        bot.send_message(chat_id, "Здесь будет информация о нашем сервисе VPN.")
+    # Обработка нажатия на кнопки способов оплаты
+    elif call.data.startswith('payment_'):
+        data = call.data.split('_')
+        if len(data) == 4:
+            payment_method = data[1]
+            months = int(data[2])
+            price = int(data[3])
+            if payment_method == "card":
+                # Ваша логика для оплаты банковской картой
+                bot.send_message(chat_id, f"Покупка на {months} месяцев успешно совершена! Оплачено банковской картой.")
+            elif payment_method == "btc":
+                # Ваша логика для оплаты через BTC
+                bot.send_message(chat_id, f"Покупка на {months} месяцев успешно совершена! Оплачено через BTC.")
+
+            # Обработка нажатия кнопки "Назад"
+            elif call.data == "back":
+                buy(call.message)
+                
+# Обработчик команды "Бесплатный тест"
+@bot.message_handler(func=lambda message: message.text == "Бесплатный тест")
+def free_trial(message):
+    # Здесь можно добавить логику для предоставления бесплатного теста
+    bot.reply_to(message, "Вы выбрали Бесплатный тест. Наслаждайтесь бесплатным тестом VPN!")
 
 # Запускаем бота
 bot.polling()
--- tg.py
+++ tg.py
@@ -1,3 +1,4 @@
+"""
 import telebot
 from telebot import types
 
@@ -12,7 +13,7 @@ bot = telebot.TeleBot(TOKEN)
 # Функция для отображения начального меню
 def display_start_menu(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buy_button = types.KeyboardButton("Купить VPN")
+    buy_button = types.KeyboardButton("Купить")
     profile_button = types.KeyboardButton("Профиль")
     free_trial_button = types.KeyboardButton("Бесплатный тест")
     markup.add(buy_button, profile_button)
@@ -43,6 +44,116 @@ def display_profile(message):
 def profile(message):
     display_profile(message)
 
+ Инлайн кнопки КУПИТЬ 
+@bot.message_handler(func=lambda message: message.text == "Купить")
+def buy(message):
+    markup = types.InlineKeyboardMarkup()
+    month_button = types.InlineKeyboardButton("1 месяц (100 руб.)", callback_data='1_month')
+    three_months_button = types.InlineKeyboardButton("3 месяца (250 руб.)", callback_data='3_months')
+    six_months_button = types.InlineKeyboardButton("6 месяцев (450 руб.)", callback_data='6_months')
+    twelve_months_button = types.InlineKeyboardButton("12 месяцев (800 руб.)", callback_data='12_months')
+    markup.row(month_button, three_months_button)
+    markup.row(six_months_button, twelve_months_button)
+    bot.send_message(message.chat.id, "Выберите тариф:", reply_markup=markup)
+
+    # Здесь можно добавить логику для покупки подписки
+    bot.reply_to(message, "Вы выбрали Купить. Покупайте подписку на VPN здесь.")
+
+
+ Инлайн кнопки КУПИТЬ 
+@bot.message_handler(func=lambda message: message.text == "Купить")
+def buy(message):
+    markup = types.InlineKeyboardMarkup()
+    month_button = types.InlineKeyboardButton("1 месяц (100 руб.)", callback_data='1_month')
+    three_months_button = types.InlineKeyboardButton("3 месяца (250 руб.)", callback_data='3_months')
+    six_months_button = types.InlineKeyboardButton("6 месяцев (450 руб.)", callback_data='6_months')
+    twelve_months_button = types.InlineKeyboardButton("12 месяцев (800 руб.)", callback_data='12_months')
+    markup.row(month_button, three_months_button)
+    markup.row(six_months_button, twelve_months_button)
+    bot.send_message(message.chat.id, "Выберите тариф:", reply_markup=markup)
+
+    # Здесь можно добавить логику для покупки подписки
+    bot.reply_to(message, "Вы выбрали Купить. Покупайте подписку на VPN здесь.")
+
+
+# Обработчик команды "Купить"
+@bot.message_handler(func=lambda message: message.text == "Купить")
+def buy(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    month_button = types.KeyboardButton("1 месяц (100 руб.)")
+    three_months_button = types.KeyboardButton("3 месяца (250 руб.)")
+    six_months_button = types.KeyboardButton("6 месяцев (450 руб.)")
+    twelve_months_button = types.KeyboardButton("12 месяцев (800 руб.)")
+    back_button = types.KeyboardButton("Назад")
+    markup.add(month_button)
+    markup.add(three_months_button)
+    markup.add(six_months_button) 
+    markup.add(twelve_months_button) 
+    markup.add(back_button)
+    bot.send_message(message.chat.id, "Выберите тариф:", reply_markup=markup)
+
+    bot.reply_to(message, "Вы выбрали Купить. Покупайте подписку на VPN здесь.")
+# Обработчик команды "Информация"
+@bot.message_handler(func=lambda message: message.text == "Информация")
+def information(message):
+    # Здесь можно добавить информацию о вашем сервисе VPN
+    bot.reply_to(message, "Вы выбрали Информация. Здесь будет информация о нашем сервисе VPN.")
+
+# Обработчик команды "Бесплатный тест"
+@bot.message_handler(func=lambda message: message.text == "Бесплатный тест")
+def free_trial(message):
+    # Здесь можно добавить логику для предоставления бесплатного теста
+    bot.reply_to(message, "Вы выбрали Бесплатный тест. Наслаждайтесь бесплатным тестом VPN!")
+
+# Запускаем бота
+bot.polling()
+
+"""
+
+import telebot
+from telebot import types
+
+# Укажите токен вашего бота
+TOKEN = '6574304289:AAHbp-YuG9efM7lV--d9nOFawwGERdRZhmg'
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot(TOKEN)
+
+# Создаем словарь для хранения информации о пользователях
+users = {}
+
+# Функция для отображения начального меню
+def display_start_menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buy_button = types.KeyboardButton("Купить")
+    profile_button = types.KeyboardButton("Профиль")
+    free_trial_button = types.KeyboardButton("Бесплатный тест")
+    markup.add(profile_button)
+    markup.add(free_trial_button)
+    markup.add(buy_button)
+    bot.send_message(message.chat.id, "👾 Приветствую! Я Ваш личный бот и помощник MaskVPN!\n \nЯ помогаю c обходом блокировок и защитой вашей конфиденциальности.", reply_markup=markup)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    display_start_menu(message)
+
+# Функция для отображения профиля
+def display_profile(message):
+    chat_id = message.chat.id
+    if chat_id not in users:
+        users[chat_id] = {"id": chat_id}
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    profile_button = types.KeyboardButton("Профиль")
+    markup.add(profile_button)
+    bot.send_message(message.chat.id, f"Ваш профиль (ID: {users[chat_id]['id']}):", reply_markup=markup)
+    bot.send_message(message.chat.id, "Дополнительные опции в профиле:", 
+                     reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton("Информация", callback_data="info")))
+
+# Обработчик команды "Профиль"
+@bot.message_handler(func=lambda message: message.text == "Профиль")
+def profile(message):
+    display_profile(message)
 
 # Обработчик команды "Купить"
 @bot.message_handler(func=lambda message: message.text == "Купить")
